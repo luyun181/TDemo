ec624e9dc0c726c24e1a32bdf2b2e8c8a964ca76
luyun181 <1039008394@qq.com>
2017-08-23 16:40:35 +0800

    zxing

--- app/src/main/java/com/demo/www/tdemo/ZxActivity.java
+++ app/src/main/java/com/demo/www/tdemo/ZxActivity.java
@@ -1,15 +1,19 @@
 package com.demo.www.tdemo;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.kcode.permissionslib.main.OnRequestPermissionsCallBack;
+import com.kcode.permissionslib.main.PermissionCompat;
 
 public class ZxActivity extends AppCompatActivity {
     private Button btn;
@@ -23,14 +27,30 @@ public class ZxActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                IntentIntegrator integrator = new IntentIntegrator(ZxActivity.this);
+                PermissionCompat.Builder builder = new PermissionCompat.Builder(context);
+                builder.addPermissions(new String[]{Manifest.permission.CAMERA});
+                builder.addPermissionRationale("say why need the permission");
+                builder.addRequestPermissionsCallBack(new OnRequestPermissionsCallBack() {
+                    @Override
+                    public void onGrant() {
+                        //do something
+                        IntentIntegrator integrator = new IntentIntegrator(ZxActivity.this);
 // 设置要扫描的条码类型，ONE_D_CODE_TYPES：一维码，QR_CODE_TYPES-二维码
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
-                integrator.setPrompt("扫描条形码");
-                integrator.setCameraId(0);  // 使用默认的相机
-                integrator.setBeepEnabled(true); // 扫到码后播放提示音
+                        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+                        integrator.setPrompt("扫描条形码");
+                        integrator.setCameraId(0);  // 使用默认的相机
+                        integrator.setBeepEnabled(true); // 扫到码后播放提示音
 //                integrator.setBarcodeImageEnabled(true);
-                integrator.initiateScan();
+                        integrator.initiateScan();
+                    }
+
+                    @Override
+                    public void onDenied(String permission) {
+                        Log.e("xxx", permission + "Denied");
+                    }
+                });
+                builder.build().request();
+
             }
         });
 
